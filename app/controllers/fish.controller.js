17e624f3b8a88977a5572321b98d76f223a8e7c4
simohelorabi <mohamed.elorabi.ma@gmail.com>
2021-03-30 19:43:25 +0800

    test

--- app/controllers/fish.controller.js
+++ app/controllers/fish.controller.js
@@ -1058,7 +1058,9 @@ exports.rewarding = async (competition) => {
         console.log('second: ', winners2)
         console.log('third: ', winners3)
 
-        if (winners1.length === 1 && winners2.length === 1) {
+        if (winners1.length === 1 && winners2 === 0) {
+            await giveReward(winners1[0].userId, competition.reward1);
+        } else if (winners1.length === 1 && winners2.length === 1) {
             await giveReward(winners1[0].userId, competition.reward1);
             await giveReward(winners2[0].userId, competition.reward2);
             if (winners3.length > 0) {
