845a424c15f9a48ce32f3a5058e4a54024da84d6
shurenyu <yupali123@outlook.com>
2021-08-25 08:08:42 +0800

    changed to test db

--- app/config/db.local.config.js
+++ app/config/db.local.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-	HOST: "localhost",
-	USER: "root",
-	PASSWORD: "",
-	DB: "fishing",
+	HOST: "13.125.198.189",
+	USER: "fishing_user",
+	PASSWORD: "20191129aA!",
+	DB: "fishing_db",
 	dialect: "mysql",
 	pool: {
 		max: 5,
