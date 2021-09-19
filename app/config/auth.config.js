29063594263b9cd866887ffdfa0538ce70a83b68
shurenyu <yupali123@outlook.com>
2021-09-19 22:37:40 +0800

    changed base url

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	// BASE_URL: "http://localhost:5000",
-	BASE_URL: "http://13.125.198.189:5000",
-	// BASE_URL: "http://54.180.210.166:5000",
+	// BASE_URL: "http://13.125.198.189:5000",
+	BASE_URL: "http://54.180.210.166:5000",
 	SECRET_KEY: "taste-of-fishing-secret-key",
 	PENDING_EXPIRATION: 600000,
 	GOOGLE_API_KEY: "AIzaSyDDu0SXRJ7mFAl0pVR2PXOm585uiMQcyrg",
